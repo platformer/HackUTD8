b73ec87c5153149e5ddb7bf81312bc6445d40ae3
platformer <andrewsenth@gmail.com>
2021-11-14 14:14:42 -0600

    final 4

--- background.js
+++ background.js
@@ -1,11 +1,8 @@
-chrome.runtime.onMessage.addListener(dupTab);
-
-async function dupTab(e)
-{
-    console.log("clicked!")
-    // let curTab = await chrome.tabs.getCurrent();
-    // await chrome.tabs.duplicate(curTab["id"]);
-    // await chrome.tabs.goBack();
-    // var ind = curTab.index + 1;
-    // await chrome.tabs.move(curTab, ind);
+chrome.runtime.onMessage.addListener(async function(msg, sender, sendResponse){
+    var curTab = sender.tab.id
+    await chrome.tabs.duplicate(curTab);
+    await chrome.tabs.goBack();
+    var ind = curTab.index + 1;
+    await chrome.tabs.move(curTab, ind);
 }
+);
