d36c8b91d869cd94f46790bf6ee4fa898befdb57
platformer <andrewsenth@gmail.com>
2021-11-14 11:36:07 -0600

    final?

--- content.js
+++ content.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+document.addEventListener('click', handleClick);
+
+async function handleClick(e)
+{
+    if (e.ctrlKey)
+    {
+        curTab = await chrome.tabs.getCurrentTab();
+        chrome.tabs.duplicate(curTab);
+        chrome.tabs.goBack();
+        curTab = await chrome.tabs.getCurrentTab();
+        ind = curTab.index - 1.
+        chrome.tabs.move(curTab, ind)
+    }
+}
