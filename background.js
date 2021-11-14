9adc2c3b5a2e59cb6b44fd90bb49553da358abc7
platformer <andrewsenth@gmail.com>
2021-11-14 14:27:57 -0600

    final final

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
-chrome.runtime.onMessage.addListener(async function(msg, sender, sendResponse){
+chrome.runtime.onMessage.addListener(async function (msg, sender, sendResponse) {
     var curTab = sender.tab.id
-    await chrome.tabs.duplicate(curTab);
-    await chrome.tabs.goBack();
-    var ind = curTab.index + 1;
-    await chrome.tabs.move(curTab, ind);
+    chrome.tabs.duplicate(curTab);
+    chrome.tabs.goBack(curTab);
 }
 );
